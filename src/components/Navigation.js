447790051fe8072849063b642f6c594dc131683e
veerce <vc2534@columbia.edu>
2023-11-28 18:37:51 -0500

    Increased size of container

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,31 +26,6 @@ const tabs = [{
     label: "Profile"
   }]
 
-// const Navigation = (props) => {
-// 	return (
-//         <nav className="navbar fixed-bottom navbar-light bottom-tab-nav" role="navigation">
-//         <Nav className="w-100">
-//           <div className=" d-flex flex-row justify-content-around w-100">
-//             {
-//               tabs.map((tab, index) =>(
-//                 <NavItem key={`tab-${index}`}>
-//                   <NavLink to={tab.route} className="nav-link bottom-nav-link" activeClassName="active">
-//                     <div className="row d-flex flex-column justify-content-center align-items-center">
-//                       <FontAwesomeIcon size="lg" icon={tab.icon}/>
-//                       <div className="bottom-tab-label">{tab.label}</div>
-//                     </div>
-//                   </NavLink>
-//                 </NavItem>
-//               ))
-//             }
-//           </div>
-//         </Nav>
-//       </nav>
-//   )
-// };
-
-// export default Navigation;
-
 const Navigation = (props) => {
   return (
     <nav className="navbar fixed-bottom navbar-light bottom-tab-nav" role="navigation">
